fccf2cd861df91856fb707afb96d3b58b1aafcd5
liqiang <liqiang05@kanzhun.com>
2021-06-22 14:10:46 +0800

    工程上传

---
+++ package/package/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  package
+//
+//  Created by lq on 2021/6/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ package/package/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  package
+//
+//  Created by lq on 2021/6/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
